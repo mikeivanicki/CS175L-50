2f201dd3495842daa4e308d8d17d5d641ee21be0
mikeivanicki <91150796+mikeivanicki@users.noreply.github.com>
2022-01-24 19:04:54 -0500

    myinfo version 3

--- myinfo.py
+++ myinfo.py
@@ -2,4 +2,6 @@
 #Michael Ivanicki
 #myinfo
 print ("My name is Michael Ivanicki")
-print ("I live at 7 Orion Court")
+print ("I live at 7 Orion Court in Flavortown, New Jersey, and my zip code is 06137")
+print ("My phone number is 732-117-5150")
+print ("My college major is Computer Science")
