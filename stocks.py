5f4e7443520e68dcfd016f8d32914854c7ef0c99
mikeivanicki <91150796+mikeivanicki@users.noreply.github.com>
2022-01-31 19:09:00 -0500

    Stocks Version 3
    Added some additional changes that were specified in class

--- stocks.py
+++ stocks.py
@@ -12,7 +12,11 @@ Stock_Sold_For=Num_Shares*Selling_Price
 Selling_Commission=Commission_Rate*Stock_Sold_For
 Total_Recieved=Stock_Sold_For-Selling_Commission
 Profit_Or_Loss=Total_Recieved-Total_Paid
-print(f'Amount paid for stock:${Amount_Paid_For_Stock:7,.2f}')
+print('What was the commission rate?:' , Commission_Rate)
+print(f'How many shares did you purchase?: {Num_Shares:,}')
+print('What was the purchase price?: $',Purchase_Price)
+print('What was the selling price?: $',Selling_Price)
+print(f'Amount paid for stock: ${Amount_Paid_For_Stock:7,.2f}')
 print(f'Commission paid on the purchase: ${Purchase_Commission:7,.2f}')
 print(f'Amount the stock sold for: ${Stock_Sold_For:7,.2f}')
 print(f'Commission paid on the sale: ${Selling_Commission:7,.2f}')
