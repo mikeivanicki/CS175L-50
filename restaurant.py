ceebd14207e8a46925e0fc12059b53b5f7ac313f
mikeivanicki <91150796+mikeivanicki@users.noreply.github.com>
2022-02-03 15:43:51 -0500

    Restaurant

---
+++ restaurant.py
@@ -0,0 +1,29 @@
+#restaurant.py
+#CS175-50L
+#Michael Ivanicki
+vegetarian=False
+vegan=False
+gluten_free=False
+
+input_1=input('Is anyone in your party vegetarian? ')
+if input_1=='yes':
+    vegetarian=True
+input_2=input('Is anyone in your party vegan? ')
+if input_2=='yes':
+    vegan=True
+input_3=input('Is anyone in your party gluten free? ')
+if input_3=='yes':
+    gluten_free=True
+    
+print('Here are your restaurant choices:')
+if vegetarian==True and vegan==False and gluten_free==False or vegetarian==False \
+   and vegan==False and gluten_free==False:
+    print('Mama\'s Fine Italian')
+if vegetarian==True and gluten_free==True and vegan==False \
+   or vegetarian==False and gluten_free==False and vegan==False \
+   or vegetarian==True and gluten_free==False and vegan==False \
+   or vegetarian==False and gluten_free==True and vegan==False:
+    print('Main Street Pizza')
+if vegetarian==False and vegan==False and gluten_free==False: 
+    print ('Joe\'s Gourmet Burgers')
+print ('Corner Cafe \nChef\'s Kitchen')    
