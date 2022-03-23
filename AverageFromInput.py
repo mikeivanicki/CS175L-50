fdcd42fb7b922c0b25038df1d0c849820d794149
Michael Ivanicki <91150796+mikeivanicki@users.noreply.github.com>
2022-03-23 18:52:16 -0400

    Average From Input (with exceptions)
    made some improvements to the exceptions so that the program skips the bad values in the value errors

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,25 +2,24 @@
 #CS175-50L
 #Michael Ivanicki
 
-def main():
+def main(): #5
     try:
         numbers_file = open('numbers.txt', 'r')
-        count = 0
+        count = 0 #10
         line = 0
-        total = 0
-        
+        total = 0 
         for line in numbers_file:
-            count = count + 1
-            total = total + float(line)
-            print (f'I read in {count} number(s) Current number is: {line} Total is: {total}')
+            try:
+                count = count + 1 #15
+                total = total + float(line)
+                print (f'I read in {count} number(s) Current number is: {line} Total is: {total}')
+            except ValueError: #20
+                print('There was an error in one of the values in numbers.txt')
         average = total / 3
         print (f'Average: {average}')
-            
         numbers_file.close()
-    except ValueError:
-        print('There was an error in one of the values in numbers.txt')
     except IOError:
         print('Error: The file numbers.txt not found')
-
 if __name__ == '__main__':
     main()
+#25
