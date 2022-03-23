a7a0d86145dbc30563efe9dc4bcf22a9f079a557
Michael Ivanicki <91150796+mikeivanicki@users.noreply.github.com>
2022-03-23 18:09:33 -0400

    Average From Input
    added Exception Handling for other assignment

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -3,19 +3,24 @@
 #Michael Ivanicki
 
 def main():
-    numbers_file = open('numbers.txt', 'r')
-    count = 0
-    line = 0
-    total = 0
-    
-    for line in numbers_file:
-        count = count + 1
-        total = total + float(line)
-        print (f'I read in {count} number(s) Current number is: {line} Total is: {total}')
-    average = total / 3
-    print (f'Average: {average}')
+    try:
+        numbers_file = open('numbers.txt', 'r')
+        count = 0
+        line = 0
+        total = 0
         
-    numbers_file.close()
+        for line in numbers_file:
+            count = count + 1
+            total = total + float(line)
+            print (f'I read in {count} number(s) Current number is: {line} Total is: {total}')
+        average = total / 3
+        print (f'Average: {average}')
+            
+        numbers_file.close()
+    except ValueError:
+        print('There was an error in one of the values in numbers.txt')
+    except IOError:
+        print('Error: The file numbers.txt not found')
 
 if __name__ == '__main__':
     main()
